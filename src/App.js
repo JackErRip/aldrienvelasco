545764024880c637424263c5ae251b63f5643317
Aldrien Velasco <136085120+JackErRip@users.noreply.github.com>
2024-05-18 22:05:19 +0800

    Updated the text and GUI Scaling and UI

--- src/App.js
+++ src/App.js
@@ -10,8 +10,14 @@ function App() {
         <div className="h1-Aldrien">
           <h1>aldrienvelasco.vercel.app</h1>
         </div>
-        <p>Welcome to my personal website.</p>
-        <p>I'm a software engineer with a passion for building web applications.</p>
+        
+        <div className="about-me">
+          <p>I'm a React Developer with nearly 2 years of experience working with React and Next.js</p>
+        </div>
+        <div className="hr-line">
+          <hr /> 
+        </div>
+        
       </header>
     </div>
   );
